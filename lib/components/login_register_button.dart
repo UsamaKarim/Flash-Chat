8b5c183a06f72549b4efcb170a1e8cbda132d308
Usama Karim <sonuosama@gmail.com>
2020-09-23 10:04:41 +0500

    Add login register button

---
+++ lib/components/login_register_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterButton extends StatelessWidget {
+  LoginRegisterButton({this.onPressed, this.text, this.color});
+
+  final Function onPressed;
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
